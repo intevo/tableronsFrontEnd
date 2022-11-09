785dd5735c393c6abb7c5b2c405be75600c09d5f
Santiago Arias <sariasj2022@gmail.com>
2022-11-09 09:46:43 -0500

    "avances de tabla"

--- js/script.js
+++ js/script.js
@@ -1,63 +1,89 @@
 window.onload = function () {
+  fetch("http://localhost:8080/factura")
+    .then((response) => response.json())
+    .then((data) => {
+      const tbody = document.getElementById("tbody");
 
-    var chart = new CanvasJS.Chart("chartContainer", {
+      data.forEach((element) => {
+        // {idFactura: 44444, fechaRegistro: '2022-09-09', fechaEntrega: '2022-08-09', valorTotal: 987000000}
+        const date = new Date(element.fechaRegistro + 'T10:20:30Z')
+        const monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+                            "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+        console.log(element)
+        console.log(monthNames[date.getMonth()])
+
+
+        var tr = document.createElement("tr");
+
+        var td = document.createElement("td");
+        var nameText = document.createTextNode(name);
+        td.appendChild(nameText);
+        tr.appendChild(td);
+      });
+
+      var chart = new CanvasJS.Chart("chartContainer", {
         animationEnabled: true,
         theme: "light2",
         title: {
-            text: "Facturación Intevo"
+          text: "Facturación Intevo",
         },
         axisX: {
-            title: "Nombre Entrega",
-            valueFormatString: "DD MMM",
-            crosshair: {
-                enabled: true,
-                snapToDataPoint: true
-            }
+          title: "Nombre Entrega",
+          valueFormatString: "DD MMM",
+          crosshair: {
+            enabled: true,
+            snapToDataPoint: true,
+          },
         },
         axisY: {
-            title: "Valor Entrega",
-            includeZero: true,
-            crosshair: {
-                enabled: true
-            }
+          title: "Valor Entrega",
+          includeZero: true,
+          crosshair: {
+            enabled: true,
+          },
         },
         toolTip: {
-            shared: true
+          shared: true,
         },
         legend: {
-            cursor: "pointer",
-            verticalAlign: "bottom",
-            horizontalAlign: "left",
-            dockInsidePlotArea: true,
-            itemclick: toogleDataSeries
+          cursor: "pointer",
+          verticalAlign: "bottom",
+          horizontalAlign: "left",
+          dockInsidePlotArea: true,
+          itemclick: toogleDataSeries,
         },
-        data: [{
+        data: [
+          {
             type: "line",
             showInLegend: true,
             name: "Facturado",
             markerType: "square",
             xValueFormatString: "DD MMM, YYYY",
             color: "#874bd7",
-            dataPoints: [                 
-                { x: new Date(2020, 11, 01), y: 1120000000},
-                { x: new Date(2020, 12, 01), y: 1895136100},
-                { x: new Date(2021, 01, 01), y: 2468058167},
-                { x: new Date(2021, 03, 01), y: 3439264400},
-                { x: new Date(2021, 04, 01), y: 3751980201},
-            ]
-        },
-        {
-        }]
-    });
-    chart.render();
+            dataPoints: [
+              { x: new Date(2020, 11, 01), y: 1120000000 },
+              { x: new Date(2020, 12, 01), y: 1895136100 },
+              { x: new Date(2021, 01, 01), y: 2468058167 },
+              { x: new Date(2021, 03, 01), y: 3439264400 },
+              { x: new Date(2021, 04, 01), y: 3751980201 },
+            ],
+          },
+          {},
+        ],
+      });
+      chart.render();
 
-    function toogleDataSeries(e) {
-        if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-            e.dataSeries.visible = false;
+      function toogleDataSeries(e) {
+        if (
+          typeof e.dataSeries.visible === "undefined" ||
+          e.dataSeries.visible
+        ) {
+          e.dataSeries.visible = false;
         } else {
-            e.dataSeries.visible = true;
+          e.dataSeries.visible = true;
         }
         chart.render();
-    }
-
-}
+      }
+    });
+};
