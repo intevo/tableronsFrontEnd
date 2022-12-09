bbb2b248720cca135fb3b5a3ce7e5308b3681370
intevo <sariasj2022@gmail.com>
2022-12-09 09:59:58 -0500

    'implementacion de decimales porcentaje'

--- js/crearANS.js
+++ js/crearANS.js
@@ -246,3 +246,81 @@ fetch('http://localhost:8080/factura')
             select.appendChild(option);
         });
     });
+
+// 
+function solonumeros(e){
+
+        key=e.keyCode || e.which;
+        teclado=String.fromCharCode(key);
+        numeros="0123456789";
+        especiales="8-37-38-46-13";
+        teclado_especial=false;
+
+ 
+        for (var i in especiales){
+            if (key==especiales[i]){
+                teclado_especial=true;
+            }
+        }
+
+        if (numeros.indexOf(teclado)==-1 && !teclado_especial){
+            return false;
+            }
+          }
+
+// DECIMALES PARA VALOR NOTA CREDITO
+
+var separador = document.getElementById('valorNotacredito');
+
+separador.addEventListener('keyup', (e) => {
+    var entrada = e.target.value.split('.').join('');
+    entrada = entrada.split('').reverse();
+    
+    var salida = [];
+    var aux = '';
+    
+    var paginador = Math.ceil(entrada.length / 3);
+    
+    for(let i = 0; i < paginador; i++) {
+        for(let j = 0; j < 3; j++) {
+            "123 4"
+            if(entrada[j + (i*3)] != undefined) {
+                aux += entrada[j + (i*3)];
+            }
+        }
+        salida.push(aux);
+        aux = '';
+       
+        e.target.value = salida.join('.').split("").reverse().join('');
+    }
+    
+}, false);
+
+// DECIMALES PARA PORCENTAJE
+
+var separador = document.getElementById('porcentaje');
+
+separador.addEventListener('keyup', (e) => {
+    var entrada = e.target.value.split('.').join('');
+    entrada = entrada.split('').reverse();
+    
+    var salida = [];
+    var aux = '';
+    
+    var paginador = Math.ceil(entrada.length / 3);
+    
+    for(let i = 0; i < paginador; i++) {
+        for(let j = 0; j < 3; j++) {
+            "123 4"
+            if(entrada[j + (i*3)] != undefined) {
+                aux += entrada[j + (i*3)];
+            }
+        }
+        salida.push(aux);
+        aux = '';
+       
+        e.target.value = salida.join('.').split("").reverse().join('');
+    }
+    
+}, false);
+
--- js/crearFacturas.js
+++ js/crearFacturas.js
@@ -99,3 +99,53 @@ function saveData() {
             select.appendChild(option);
         });
     });
+
+  //
+    
+  function solonumeros(e){
+
+        key=e.keyCode || e.which;
+        teclado=String.fromCharCode(key);
+        numeros="0123456789";
+        especiales="8-37-38-46-13";
+        teclado_especial=false;
+
+ 
+        for (var i in especiales){
+            if (key==especiales[i]){
+                teclado_especial=true;
+            }
+        }
+
+        if (numeros.indexOf(teclado)==-1 && !teclado_especial){
+            return false;
+            }
+          }
+
+  //
+
+  var separador = document.getElementById('facturaTotal');
+
+separador.addEventListener('keyup', (e) => {
+    var entrada = e.target.value.split('.').join('');
+    entrada = entrada.split('').reverse();
+    
+    var salida = [];
+    var aux = '';
+    
+    var paginador = Math.ceil(entrada.length / 3);
+    
+    for(let i = 0; i < paginador; i++) {
+        for(let j = 0; j < 3; j++) {
+            "123 4"
+            if(entrada[j + (i*3)] != undefined) {
+                aux += entrada[j + (i*3)];
+            }
+        }
+        salida.push(aux);
+        aux = '';
+       
+        e.target.value = salida.join('.').split("").reverse().join('');
+    }
+    
+}, false);
